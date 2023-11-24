ccf5092cf5ca08bea5860a80605f8268c816b4ed
Dan D <ddenholm67@hotmail.co.uk>
2023-11-24 17:56:48 +0000

    UI basic interaction

---
+++ Tests/DDZondaTechTest/UITests/Page Elements Repo/SubdivisionDisplayPageRepo.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITests.Page_Elements
+{
+    public class SubdivisionDisplayPageRepo
+    {
+        public const string StatusCodeFilterDropdown = "/html/body/div/div/div/div[1]/div[1]/button"; //Xpath is extremely volatile
+    }
+}
---
+++ Tests/DDZondaTechTest/UITests/Page Objects/SubdivisionDisplayPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using UITests.Page_Elements;
+
+namespace UITests
+{
+    public class SubdivisionDisplayPage
+    {
+        private IWebDriver driver;
+
+        private By statusCodeFilterDropDownLocator = By.XPath(SubdivisionDisplayPageRepo.StatusCodeFilterDropdown);
+       
+
+        public SubdivisionDisplayPage(IWebDriver driver) 
+        {
+            this.driver = driver;
+        }
+
+        public void ClickStatusCodeFilterDropDown()
+        {
+            driver.FindElement(statusCodeFilterDropDownLocator).Click();
+        }
+
+
+    }
+}
